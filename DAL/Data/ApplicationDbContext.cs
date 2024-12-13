d3a513aa3d6a2155c92e1e9191ddf25ba7f018c0
Starholme <Russel.delainey@gmail.com>
2024-12-13 13:06:20 -0600

    Add instances to db

--- DAL/Data/ApplicationDbContext.cs
+++ DAL/Data/ApplicationDbContext.cs
@@ -11,7 +11,9 @@ namespace DAL.Data
         {
         }
 
-        public required DbSet<Vault> Vaults { get; set; }
+        public required DbSet<Instance> Instances { get; set; }
         public required DbSet<ItemStack> Items { get; set; }
+        public required DbSet<Vault> Vaults { get; set; }
+
     }
 }
---
+++ DAL/Data/Migrations/20241213011653_Instance.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Instance : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Instances",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClusterInstanceId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VaultId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Instances", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Instances_Vaults_VaultId",
+                        column: x => x.VaultId,
+                        principalTable: "Vaults",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Instances_VaultId",
+                table: "Instances",
+                column: "VaultId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Instances");
+
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "Items");
+        }
+    }
+}
